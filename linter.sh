a91e1a33537ec20acad8b5f9abcfe26f19f40dc0
Angus Griffith <16sn6uv@gmail.com>
2013-06-08 13:57:49 +1000

    initial commit

---
+++ linter.sh
@@ -0,0 +1,7 @@
+for f in $(find . -name "*.py")
+do
+    scores=$(pylint $f | grep 'Your code has been rated at' | grep -oP '\-?[0-9.]+/10')
+    old=$(echo $scores | cut -d' ' -f2)
+    new=$(echo $scores | cut -d' ' -f1)
+    echo $f $old "->" $new
+done
